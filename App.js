600f7bd5edc01f1deb9c016a45183ad3d7018c78
Olzhas <oljasalidarov@gmail.com>
2022-04-07 12:53:41 +0600

    carousel + accordion

--- App.js
+++ App.js
@@ -7,53 +7,19 @@ import {
   Button,
   Alert,
   TextInput,
+  ScrollView
 } from "react-native";
 import { t } from "react-native-tailwindcss";
-import { Navbar } from "./components/Navbar"
+import { Navbar } from "./components/Navbar";
+import { MainContent } from "./components/MainContent";
 
 export default function App() {
-  const [counter, setText] = useState(0);
-  const [text, anotherText] = useState("Change Me");
-  const [marg, changeMarg] = useState(0);
-  var [inputText, changeInput] = useState("Hello");
-  function counterInc() {
-    setText(counter + 1);
-  }
-  function changeText() {
-    anotherText("Измененный текст");
-    changeMarg(20);
-  }
-  function onChangeInput() {
-    changeInput(event.target.value);
-  }
   return (
     <>
+    <ScrollView>
       <Navbar title="Navbar title"/>
-      <View style={styles.container}>
-        <Text style={[t.bgBlack, t.textWhite]}>ansmd,anm,das!</Text>
-        <Image source={require("./assets/favicon.png")} />
-        <Image
-          source={{
-            uri: "https://d33wubrfki0l68.cloudfront.net/554c3b0e09cf167f0281fda839a5433f2040b349/ecfc9/img/header_logo.svg",
-          }}
-          style={[t.w10, t.h10]}
-        />
-        <View style={[t.flex, t.flexRow, t.itemsCenter, t.mY10]}>
-          <Text>First </Text>
-          <Text>Second</Text>
-          <Button
-            title="Click me"
-            onPress={() => Alert.alert("Кнопка нажата")}
-          />
-        </View>
-        <View>
-          <Button onPress={counterInc} title="Counter" />
-          <Text style={{ margin: marg }}>{counter}</Text>
-          <Text onPress={changeText}>{text}</Text>
-        </View>
-        <TextInput onChange={onChangeInput} value={inputText} />
-        <Text>{inputText}</Text>
-      </View>
+      <MainContent />
+    </ScrollView>
     </>
   );
 }
